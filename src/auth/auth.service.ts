78c13dbd520aa7e97bb4355cfb665781767097df
Klaus-gudy <klausongudy@gmail.com>
2026-01-04 20:47:15 +0300

    Added controller and service for authenticatiion

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
+import { RegisterDto } from './dto/register.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+    @InjectRepository(User)
+    private readonly userRepo: Repository<User>,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async register(dto: RegisterDto) {
+    const existing = await this.userRepo.findOne({
+      where: { email: dto.email },
+    });
+
+    if (existing) {
+      throw new BadRequestException('Email already in use');
+    }
+
+    const passwordHash = await bcrypt.hash(dto.password, 10);
+
+    const user = this.userRepo.create({
+      email: dto.email,
+      passwordHash,
+    });
+
+    await this.userRepo.save(user);
+
+    return this.signToken(user);
+  }
+
+  private signToken(user: User) {
+    const payload = {
+      sub: user.id,
+      email: user.email,
+    };
+
+    return {
+      accessToken: this.jwtService.sign(payload),
+    };
+  }
+}
