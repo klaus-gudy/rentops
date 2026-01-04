3a94e1ce2aae258180255a78076ee3330b7ed858
Klaus-gudy <klausongudy@gmail.com>
2026-01-04 17:25:44 +0300

    🧪 Tried to combine both auth and user model logics

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -9,4 +10,18 @@ export class UsersService {
         @InjectRepository(User)
         private userRepository: Repository<User>,
     ) { }
+    async createUser(email: string, passwordHash: string, firstName: string, lastName: string): Promise<User> {
+        const existing = await this.userRepository.findOne({ where: { email } });
+        if (existing) {
+            throw new BadRequestException('Email already exists');
+        }
+        const hashedPassword = await bcrypt.hash(passwordHash, 10);
+        const newUser = this.userRepository.create({
+            email,
+            passwordHash: hashedPassword,
+            firstName,
+            lastName,
+        });
+        return this.userRepository.save(newUser);
+    }
 }
