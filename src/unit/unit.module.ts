54ff2131fc767d9b13265a7663a217b7e3e45214
Klaus-gudy <klausongudy@gmail.com>
2026-01-13 02:22:55 +0300

    Imported property entity to be used by unit module NB: we have imported the entity not the module

--- src/unit/unit.module.ts
+++ src/unit/unit.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UnitService } from './unit.service';
 import { UnitController } from './unit.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Unit } from './entities/unit.entity';
+import { Property } from 'src/property/entities/property.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Unit, Property]),
+  ],
   providers: [UnitService],
   controllers: [UnitController]
 })
