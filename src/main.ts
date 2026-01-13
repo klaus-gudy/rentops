51fc44dc064df39b7d69ff1046a2f2ef5428f38a
Klaus-gudy <klausongudy@gmail.com>
2026-01-13 15:06:18 +0300

    Enabled global validation pipe to vlaidate the request body

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SilentLogger } from './common/middlewares/silent-logger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: new SilentLogger(),
   });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
